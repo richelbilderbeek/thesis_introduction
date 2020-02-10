c56b2625fe77f03cc203e9e463e716aa4462d4c0
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-10 11:44:03 +0100

    FUCA story done

--- create_figures.R
+++ create_figures.R
@@ -14,7 +14,6 @@ suppressMessages(library(ggtree))
 #  -+--+-----+--+---- (time)
 #   t0 t1    t2 t3
 #
-
 ggtree::ggtree(
   ape::read.tree(text = "((C:2,B:2):1, A:2):1;"),
   ladderize = FALSE
@@ -31,7 +30,7 @@ ggtree::ggtree(
     aes(x = x, y = y, label = label),
     label.size = 0.0,
     nudge_x = 0,
-    nudge_y = -0.18,
+    nudge_y = -0.185,
     cex = 10
   ) +
   ggplot2::geom_label(
@@ -41,4 +40,79 @@ ggtree::ggtree(
     nudge_x = 0.1,
     nudge_y = 0.0,
     cex = 10
-  ) + ggplot2::ggsave(filename = "t_4.png", width = 7, height = 4)
+  ) + ggplot2::ggsave(filename = "t_3.png", width = 7, height = 4)
+
+
+
+#
+#     ----- FUCA
+#
+#  ---+---- (time)
+#     t0
+#
+
+ggtree::ggtree(
+  ape::read.tree(text = "(FUCA:0):1;"),
+  ladderize = FALSE
+) + ggtree::geom_tiplab(cex = 10) +
+  ggtree::geom_rootedge(rootedge = 1) +
+  ggtree::geom_treescale(width = 6, x = -2, fontsize = 0) +
+  ggplot2::geom_vline(xintercept = -1, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 0, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 1, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 2, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 3, lty = "dotted") +
+  ggplot2::geom_label(
+    data = data.frame(x = seq(-1, 3), y = 0, label = paste0("t", seq(0,4))),
+    aes(x = x, y = y, label = label),
+    label.size = 0.0,
+    nudge_x = 0,
+    nudge_y = -0.185,
+    cex = 10
+  ) +
+  ggplot2::geom_label(
+    data = data.frame(x = 4, y = 0, label = "t"),
+    aes(x = x, y = y, label = label),
+    label.size = 0.0,
+    nudge_x = 0.1,
+    nudge_y = 0.0,
+    cex = 10
+  ) + ggplot2::ggsave(filename = "t_0.png", width = 7, height = 4)
+
+
+#
+#      +---- A
+#   ---+
+#      +---- B
+#
+#  -+--+--- (time)
+#   t0 t1
+#
+
+ggtree::ggtree(
+  ape::read.tree(text = "(B:1,A:1);"),
+  ladderize = FALSE
+) + ggtree::geom_tiplab(cex = 10) +
+  ggtree::geom_rootedge(1) +
+  ggtree::geom_treescale(width = 6, x = -2, fontsize = 0) +
+  ggplot2::geom_vline(xintercept = -1, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 0, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 1, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 2, lty = "dotted") +
+  ggplot2::geom_vline(xintercept = 3, lty = "dotted") +
+  ggplot2::geom_label(
+    data = data.frame(x = seq(-1, 3), y = 0, label = paste0("t", seq(0,4))),
+    aes(x = x, y = y, label = label),
+    label.size = 0.0,
+    nudge_x = 0,
+    nudge_y = -0.185,
+    cex = 10
+  ) +
+  ggplot2::geom_label(
+    data = data.frame(x = 4, y = 0, label = "t"),
+    aes(x = x, y = y, label = label),
+    label.size = 0.0,
+    nudge_x = 0.1,
+    nudge_y = 0.0,
+    cex = 10
+  ) + ggplot2::ggsave(filename = "t_1.png", width = 7, height = 4)
