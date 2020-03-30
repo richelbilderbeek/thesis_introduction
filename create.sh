20c3bf0437a8b05a6acfff90721b484b178e7ac7
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-30 13:23:50 +0200

    Works

---
+++ create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pdflatex header --shell-escape
+bibtex header
+pdflatex header --shell-escape
+pdflatex header --shell-escape
+rm header.aux
+rm header.bbl
+rm header.blg
+rm header.log
+rm header.out
+mv header.pdf introduction.pdf
